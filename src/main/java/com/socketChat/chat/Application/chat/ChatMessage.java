f401609d0223435a93e73b6a41a172b95ea19977
vyomgoyal <vyom.is22@bmsce.ac.in>
2024-09-20 12:37:04 +0530

    Chat Application Done

---
+++ src/main/java/com/socketChat/chat/Application/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.socketChat.chat.Application.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/socketChat/chat/Application/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.socketChat.chat.Application.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
